caaf48a8ad84331122767ab1bc276d8fe71cf990
wavfunction <39447050+wavfunction@users.noreply.github.com>
2018-07-22 14:52:25 -0400

    Made some changes to make the form more intuitive

--- public_html/scripts/validator.js
+++ public_html/scripts/validator.js
@@ -11,7 +11,7 @@
         var phone = document.getElementById("contact");
         
         try{
-			//checks to see if a radio button has been toggled
+            //checks to see if a radio button has been toggled
             for (i = 0; i < radioButtons.length; i++)
             {
                 if(radioButtons[i].checked){
@@ -19,7 +19,7 @@
                 }               
             }
 			
-			//checks username input against pattern
+            //checks username input against pattern
             var pattern = /^[a-zA-Z0-9]+$/;
             var output = pattern.test(user.value);
             
@@ -30,7 +30,8 @@
                 throw "The username field is required.";
             }
             
-            if (user.length < 5){
+            //checks username length
+            if (user.value.length < 5){
                 showCheckX("x", "check");
                 throw "Username must be longer than 5 characters.";
             }
@@ -46,7 +47,7 @@
                 throw "Password needs to be at least 5 characters.";
             }
             
-			//compares confirmation with entered password
+            //compares confirmation with entered password
             if (confirmpassword.value === password.value){
                 showCheck("check2", "x2");
             } else {
@@ -61,7 +62,7 @@
                 throw "You must select a gender.";
             }
             
-			//checks email input against pattern
+            //checks email input against pattern
             var pattern = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}/igm;
             var output = pattern.test(email.value);
                 
@@ -73,7 +74,7 @@
                 throw "Invalid email address.";
             }
             
-			//checks phone input against pattern
+            //checks phone input against pattern
             var pattern = /^\d{10}$/;
             var output = pattern.test(phone.value);
            
@@ -84,26 +85,26 @@
                 showCheckX("x5", "check5");
                 throw "Phone number must be exactly 10 digits.";
             }
-            
-			//sets checkmark if input is correct
+           
+            //sets checkmark if input is correct
             function showCheck(check, x){
                 document.getElementById(check).style.visibility = "visible";
                 document.getElementById(x).style.visibility = "hidden";
-            };
+            }
             
-			//sets X if input is incorrect
+            //sets X if input is incorrect
             function showCheckX(x, check){
                 document.getElementById(x).style.visibility = "visible";
                 document.getElementById(check).style.visibility = "hidden";
-            };
+            }
 			
-			//clears the error message content from innerHTML
+            //clears the error message content from innerHTML
             function Clear() {
                 msg.innerHTML = "";
                 msg.style.color = "red";    
-            };
+            }
         
-		//form cannot be submitted if boolean isValid is false
+        //form cannot be submitted if boolean isValid is false
         }catch(err){
                 msg.innerHTML = (err);
                 msg.style.color = "red";
